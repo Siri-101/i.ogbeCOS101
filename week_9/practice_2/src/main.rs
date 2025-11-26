1d9d4b33e027db773e598da348445ee28fd97ef4
Siri-101 <iyeresiri.ogbe@pau.edu.ng>
2025-11-26 16:05:41 +0100

    reading a text file and printing to console

---
+++ week_9/practice_2/src/main.rs
@@ -0,0 +1,10 @@
+use std::io::Write;
+use std::io::Read;
+
+fn main() {
+    let mut file = std::fs::File::open("welcome_message.txt").unwrap();
+
+    let mut content = String::new();
+    file.read_to_string(&mut content).expect("failed to read");
+    println!("{}", content);
+}
