4c5ba13b90aa5925faac51b946c9122d4f01a79f
Siri-101 <iyeresiri.ogbe@pau.edu.ng>
2025-11-27 20:02:56 +0100

    Adding number rows

--- week_9/project_2/src/main.rs
+++ week_9/project_2/src/main.rs
@@ -4,9 +4,9 @@ use std::io::Write;
 fn main() {
     let mut file = std::fs::File::create("data.csv").unwrap();
     file.write_all(" , , PAU SMIS \n ".as_bytes()).expect("Failed to write file");
-    file.write_all("Student Name, Matric. Number, Department, Level \n".as_bytes()).expect("Failed to write file");
+    file.write_all("S/N, Student Name, Matric. Number, Department, Level \n".as_bytes()).expect("Failed to write file");
 
-    let name = vec!["Oluchi Mordi,", "Adams Aliyu,", "Shania Bolade,", "Adekunle Gold,", "Blanca Edemoh,"];
+    let name = vec![",Oluchi Mordi,", ",Adams Aliyu,", ",Shania Bolade,", ",Adekunle Gold,", ",Blanca Edemoh,"];
     let matric_num = vec!["ACC10211111,", "ECO10110101,", "CSC10328828,", "EEE11020202,", "MEE10202001,"];
     let departmnent = vec!["Accounting,", "Economics,", "Computer,", "Electrical,", "Mechanical,"];
     let level = vec!["300,", "100,", "200,", "200,", "100,"];
@@ -25,6 +25,7 @@ fn main() {
     //     Williams".as_bytes()).expect("Failed to edit");
     let mut index = 0;
     for i in &name {
+        file.write_all((index+1).to_string().as_bytes()).expect("Failed to write file");
         file.write_all((name[index]).as_bytes()).expect("Failed to write file");
         file.write_all((matric_num[index]).as_bytes()).expect("Failed to write file");
         file.write_all((departmnent[index]).as_bytes()).expect("Failed to write file");
